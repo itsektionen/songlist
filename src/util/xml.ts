c7d7689e1592083c20c24622bb96c76841fa8ec5
Hugo <54435874+Cactooz@users.noreply.github.com>
2025-12-03 12:56:19 +0100

    Fix formatting

--- src/util/xml.ts
+++ src/util/xml.ts
@@ -49,7 +49,7 @@ function songContentToXml(content: Song['content']): string {
 				paragraph
 					.split('\n')
 					.map((s) => s.substring(2))
-					.join('\n')
+					.join('\n'),
 			)}</comment>\n`;
 		} else {
 			xml += `\t\t<p>${xmlifyParagraph(paragraph)}</p>\n`;
