1078536d93e9a94b3bc72a9ab20d749f2e299fa5
Hugo <54435874+Cactooz@users.noreply.github.com>
2025-12-29 16:32:34 +0100

    Bump dependencies & fix build formatting (#59)
    Bump to version 2.2.1

--- src/util/createSongs.ts
+++ src/util/createSongs.ts
@@ -2,7 +2,6 @@ import { buildXmlString } from './xml';
 import { getAllSongs, sortSongs } from './songs';
 import { Song } from '../definitions/song';
 import { formatAuthor } from './formatAuthor';
-import { format } from 'prettier';
 import dayjs from 'dayjs';
 
 export default function createSongs(updatedAt: string = dayjs().format('YYYY-MM-DD')) {
@@ -17,8 +16,5 @@ export function createJsonSongs(songs: Song[], updatedAt: string) {
 	const sortedSongs = sortSongs(songs).map(({ sorting, author, ...song }) => {
 		return { ...song, author: author ? formatAuthor(author) : undefined };
 	});
-	return format(JSON.stringify({ songs: sortedSongs, updatedAt }), {
-		parser: 'json',
-		useTabs: true,
-	});
+	return JSON.stringify({ songs: sortedSongs, updatedAt });
 }
