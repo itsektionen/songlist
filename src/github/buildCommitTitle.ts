1554522f2433b0f761b1529b47957a91bcc56f09
Hugo <54435874+Cactooz@users.noreply.github.com>
2026-01-30 13:09:32 +0100

    Fix building on no song changes

--- src/github/buildCommitTitle.ts
+++ src/github/buildCommitTitle.ts
@@ -5,6 +5,6 @@ import timezone from 'dayjs/plugin/timezone';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-export default function () {
-	return dayjs().tz('Europe/Stockholm').format('[Build for changes made at] YYYY-MM-DD HH:mm:ss');
-}
+console.log(
+	`out=${dayjs().tz('Europe/Stockholm').format('[Build for changes made at] YYYY-MM-DD HH:mm:ss')}`,
+);
